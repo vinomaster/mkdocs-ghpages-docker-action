6600b07fdbe43783363b511dc6c34170748af320
Vinomaster <dan.gisolfi@gmail.com>
2020-06-19 20:17:22 -0400

    new weasy base

--- Dockerfile
+++ Dockerfile
@@ -1,8 +1,27 @@
-# Container image OS
-FROM alpine:3.10
+#FROM squidfunk/mkdocs-material:5.3.0
+FROM minidocks/mkdocs
+MAINTAINER gisolfi@us.ibm.com
 
-# Copies action repository code file to the filesystem path `/` of the container
-COPY entrypoint.sh /entrypoint.sh
+#----------------------------------------------------
+# Install MkDocs environment w/complimentary tools
+#----------------------------------------------------
 
-# Code file to execute when the docker container starts up (`entrypoint.sh`)
-ENTRYPOINT ["/entrypoint.sh"]
+#RUN apk add gcc-plugin-devel python-devel python-six python-pygments graphviz
+#RUN apk --update --upgrade add bash cairo pango gdk-pixbuf py3-cffi py3-pillow py-lxml
+#RUN pip3 install --upgrade pip
+#RUN pip3 install weasyprint
+
+#RUN pip3 install mkpdfs-mkdocs==1.0.1
+#RUN pip3 install mkdocs-pdf-export-plugin
+
+ENV ENABLE_PDF_EXPORT=0
+
+#----------------------------------------------------
+# Prepare GitHub Action
+#----------------------------------------------------
+
+COPY action.sh /action.sh
+
+RUN apk add --no-cache bash && chmod +x /action.sh
+
+ENTRYPOINT ["/action.sh"]
---
+++ action.sh
@@ -0,0 +1,43 @@
+#!/bin/bash
+
+set -e
+
+function print_info() {
+    echo -e "\e[36mINFO: ${1}\e[m"
+}
+
+if [ -n "${REQUIREMENTS}" ] && [ -f "${GITHUB_WORKSPACE}/${REQUIREMENTS}" ]; then
+    pip install -r "${GITHUB_WORKSPACE}/${REQUIREMENTS}"
+else
+    REQUIREMENTS="${GITHUB_WORKSPACE}/requirements.txt"
+    if [ -f "${REQUIREMENTS}" ]; then
+        pip install -r "${REQUIREMENTS}"
+    fi
+fi
+
+if [ -n "${CUSTOM_DOMAIN}" ]; then
+    print_info "Setting custom domain for github pages"
+    echo "${CUSTOM_DOMAIN}" > "${GITHUB_WORKSPACE}/docs/CNAME"
+fi
+
+if [ -n "${GITHUB_TOKEN}" ]; then
+    print_info "setup with GITHUB_TOKEN"
+    remote_repo="https://x-access-token:${GITHUB_TOKEN}@github.com/${GITHUB_REPOSITORY}.git"
+
+elif [ -n "${PERSONAL_TOKEN}" ]; then
+    print_info "setup with PERSONAL_TOKEN"
+    remote_repo="https://x-access-token:${PERSONAL_TOKEN}@github.com/${GITHUB_REPOSITORY}.git"
+fi
+
+if ! git config --get user.name; then
+    git config --global user.name "${GITHUB_ACTOR}"
+fi
+
+if ! git config --get user.email; then
+    git config --global user.email "${GITHUB_ACTOR}@users.noreply.github.com"
+fi
+
+git remote rm origin
+git remote add origin "${remote_repo}"
+
+mkdocs gh-deploy --config-file "${GITHUB_WORKSPACE}/mkdocs.yml" --force
